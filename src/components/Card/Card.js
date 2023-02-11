f7c14016374d30c0ca3ebd1879203d9315d23c34
Dzhanik Dzanagov <deicideand@yandex.ru>
2023-02-11 18:40:07 +0300

    corrected commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,11 @@
 import './Card.css';
-import { Image, Typography, Rate} from 'antd';
+import { Image, Rate } from 'antd';
 import React from 'react';
 
 import notImg from '../../img/notImg.png';
 import { GenresConsumer } from '../Genres-context';
 
-const { Title, Text, Paragraph } = Typography;
-
 export default class Card extends React.Component {
-
   state = {
     words: 0,
   };
@@ -23,10 +20,10 @@ export default class Card extends React.Component {
 
   componentDidMount() {
     this.resizingElem();
-  };
+  }
 
-  componentDidUpdate (prevProps) {
-    const {widthWindow} = this.props;
+  componentDidUpdate(prevProps) {
+    const { widthWindow } = this.props;
 
     if (widthWindow !== prevProps.widthWindow) {
       this.resizingElem();
@@ -39,14 +36,14 @@ export default class Card extends React.Component {
     const card = this.heightCard.current.offsetHeight;
 
     const numberOfWordsinLines = widthDescription / 5.9;
-    const heightDescription = card - header - 70; 
+    const heightDescription = card - header - 70;
     const numberOfLines = Math.round(heightDescription / 22);
     const numberOfWords = numberOfLines * numberOfWordsinLines;
-    this.setState({words: numberOfWords});
+    this.setState({ words: numberOfWords });
   };
 
   ApdateWord = (text) => {
-    const {words} = this.state;
+    const { words } = this.state;
     const newText = text.slice(0, words);
     const newArray = newText.split(' ');
     newArray.pop();
@@ -54,14 +51,16 @@ export default class Card extends React.Component {
     return newWords;
   };
 
-  render () {
-    const {title, poster, date, overview, vote, id, onChangeRating, ratedIdMovies, genreID} = this.props;
-    
+  render() {
+    const { title, poster, date, overview, vote, id, onChangeRating, ratedIdMovies, genreID } = this.props;
+
     let currentRating = 'card__current-rating';
 
-    const image = poster ? 
-      <Image alt="movies" src={`https://image.tmdb.org/t/p/original${poster}`}/> :
-      <Image alt="error" src={notImg}/>;
+    const image = poster ? (
+      <Image alt="movies" src={`https://image.tmdb.org/t/p/original${poster}`} />
+    ) : (
+      <Image alt="error" src={notImg} />
+    );
 
     if (vote >= 0 && vote < 4) {
       currentRating += ' card__color-red';
@@ -77,32 +76,35 @@ export default class Card extends React.Component {
     }
     return (
       <GenresConsumer>
-        {
-          (genres) => <div className="card" ref={this.heightCard}>
-            <div className='card__image'>
-              {image}
-            </div>
-            <div className='card__details'>
-              <div className='card__header' ref={this.heightHeader}>
-                <div className='card__title'>
-                  <Title level={2} className="card__title">{title}</Title>
+        {(genres) => (
+          <div className="card" ref={this.heightCard}>
+            <div className="card__image">{image}</div>
+            <div className="card__details">
+              <div className="card__header" ref={this.heightHeader}>
+                <div className="card__wrapper">
+                  <h2 className="card__title">{title}</h2>
                   <div className={currentRating}>{vote.toFixed(1)}</div>
                 </div>
-                <div className='card__date'><Text type="secondary">{date}</Text></div>
-                <div className='card__genre'>
-                  {genreID.map((genreIds) => <div key={genreIds}><Text code>{genres.map((j) => j.id === genreIds ? j.name : null)}</Text></div>)}
+                <div className="card__date">
+                  <span type="secondary">{date}</span>
+                </div>
+                <div className="card__genre">
+                  {genreID.map((genreIds) => (
+                    <div key={genreIds} className="card__span">
+                      <span>{genres.map((j) => (j.id === genreIds ? j.name : null))}</span>
+                    </div>
+                  ))}
                 </div>
               </div>
-              <div className='card__description' ref={this.heightDescription}>
-                <Paragraph>{this.ApdateWord(overview)} ...
-                </Paragraph>
+              <div className="card__description" ref={this.heightDescription}>
+                <p className="card__text">{this.ApdateWord(overview)} ...</p>
               </div>
-              <div ref={this.heightRating} className='card__rating'>
-                <Rate  allowHalf value={ratedIdMovies[id]} count={10} onChange={(ev) => onChangeRating(ev, id)}/>
+              <div ref={this.heightRating} className="card__rating">
+                <Rate allowHalf value={ratedIdMovies[id]} count={10} onChange={(ev) => onChangeRating(ev, id)} />
               </div>
-            </div> 
+            </div>
           </div>
-        }
+        )}
       </GenresConsumer>
     );
   }
