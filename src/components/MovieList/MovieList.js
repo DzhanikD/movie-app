726abf67490f01a6f1a9e5f7c4d27176d22868f3
Dzhanik Dzanagov <deicideand@yandex.ru>
2023-02-11 19:10:43 +0300

    corrected spinner

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { format } from 'date-fns';
-import {Spin, Alert} from 'antd';
+import { Spin, Alert } from 'antd';
 
 import Card from '../Card';
 import './MovieList.css';
 
-function MovieList ({body, loading, error, onChangeRating, ratedIdMovies, notFound, widthWindow}) {
-  const content = !loading && !error && !notFound ? <div className='movie-list'>{body.map((el) =>
-    <Card key={el.id} onChangeRating={onChangeRating} title={el.original_title} genreID={el.genre_ids} poster={el.poster_path}
-      overview={el.overview} vote={el.vote_average} id={el.id} rate={el.rating} ratedIdMovies={ratedIdMovies} widthWindow={widthWindow}
-      date={el.release_date ? format(new Date(el.release_date), 'MMMM d, yyyy') : null}/>)}
-  </div> : null;
-  const spinner = loading ? <Spin tip="Loading" size="large" /> : null;
-  const errorMessage = error ? <Alert message="Упс!" description="Что-то пошло нет так, попробуйте снова!=)" type="error"
-    showIcon/> : null;
-  const notFoundMessage = notFound ? <Alert message='Ой=(' description="По вашему запросу ничего не найдено, попробуйте снова!" 
-    type="warning" showIcon/> : null;
+function MovieList({ body, loading, error, onChangeRating, ratedIdMovies, notFound, widthWindow }) {
+  const content =
+    !loading && !error && !notFound ? (
+      <div className="movie-list">
+        {body.map((el) => (
+          <Card
+            key={el.id}
+            onChangeRating={onChangeRating}
+            title={el.original_title}
+            genreID={el.genre_ids}
+            poster={el.poster_path}
+            overview={el.overview}
+            vote={el.vote_average}
+            id={el.id}
+            rate={el.rating}
+            ratedIdMovies={ratedIdMovies}
+            widthWindow={widthWindow}
+            date={el.release_date ? format(new Date(el.release_date), 'MMMM d, yyyy') : null}
+          />
+        ))}
+      </div>
+    ) : null;
+  const spinner = loading ? <Spin className="spinner" tip="Loading" size="large" /> : null;
+  const errorMessage = error ? (
+    <Alert message="Упс!" description="Что-то пошло нет так, попробуйте снова!=)" type="error" showIcon />
+  ) : null;
+  const notFoundMessage = notFound ? (
+    <Alert
+      message="Ой=("
+      description="По вашему запросу ничего не найдено, попробуйте снова!"
+      type="warning"
+      showIcon
+    />
+  ) : null;
 
   return (
     <>
@@ -26,7 +49,5 @@ function MovieList ({body, loading, error, onChangeRating, ratedIdMovies, notFou
     </>
   );
 }
-  
-
 
 export default MovieList;
